a3d5e8f036dff1855f4c41d4b1976958f4ad77b1
PolaSuchitra <158476040+PolaSuchitra@users.noreply.github.com>
2024-11-18 16:23:41 +0530

    Delete DND.py

--- DND.py
+++
@@ -1,9 +0,0 @@
-N,m = map(int,input().split())
-arr = list(map(int,input().split()))
-sumd, sumn = 0, 0
-for i in range(N):
-    if(arr[i]%m==0):
-        sumd+=arr[i]
-    else:
-        sumn+=arr[i]
-print(sumd - sumn)
